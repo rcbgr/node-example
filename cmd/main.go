9afaed15d91bc152ad3d514ef983967cf9cdd8a3
Ryan Nitz <ryan.nitz@coinbase.com>
2022-09-19 23:54:45 -0400

    Dev

--- cmd/main.go
+++ cmd/main.go
@@ -2,43 +2,46 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/google/uuid"
-
-	nodec "github.com/rcbgr/node-example/pkg/node"
-	"github.com/rcbgr/node-example/pkg/node/model"
+	"github.com/rcbgr/node-example/pkg/node"
 )
 
 func main() {
 
-	config := &model.NodeConfig{
-		Username: os.Getenv("NODE_USERNAME"),
-		Password: os.Getenv("NODE_PASSWORD"),
-		Endpoint: os.Getenv("NODE_ENDPOINT"),
-	}
+	usdcContract := "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 
-	req := &model.NodeAdvancedApiRequest{
-		Config: config,
-		Body: &model.JsonRpcBody{
-			Id:             uuid.New().String(),
-			JsonRpcVersion: "2.0",
-			Method:         "coinbaseCloud_getSingleBalance",
-			Params: &model.NodeAdvancedApiGetSingleBalanceRequest{
-				Address:    "0x0a59649758aa4d66e25f08dd01271e891fe52199", // Maker: PSM-USDC-A
-				Contract:   "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // USDC Contract
-				Blockchain: "Ethereum",
-				Network:    "Mainnet",
-			},
-		},
-	}
+	contract := "0x60e4d786628fea6478f785a6d7e704777c86a7c6"
 
-	res, err := nodec.NodeAdvancedApiCall(req)
+	tokens, err := node.NodeNftApiTokensByContract(contract)
 	if err != nil {
-		log.Fatalf("Failed to call coinbaseCloud_getSingleBalance: %v", err)
+		fmt.Println(err)
 	}
 
-	fmt.Println(string(res.Body))
+	fmt.Println(len(tokens))
+
+	fmt.Println(fmt.Sprintf("Tokens: %d", len(tokens)))
+
+	var owners []string
+
+	for _, token := range tokens {
+		owner, err := node.NodeNftApiTokenOwnerByContractAndToken(contract, token)
+		if err != nil {
+			fmt.Println(err)
+		}
+		owners = append(owners, owner)
+	}
+
+	fmt.Println(fmt.Sprintf("Owner addresses: %d", len(owners)))
+
+	for _, owner := range owners {
+
+		v, err := node.NodeAdvancedApiBalanceByContract(usdcContract, owner)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(fmt.Sprintf("USDC balance: %f", v))
+
+	}
 
 }
