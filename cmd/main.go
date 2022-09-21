257e85f0e983562d6e037175a14980fe71e2ae96
Ryan Nitz <ryan.nitz@coinbase.com>
2022-09-20 20:25:45 -0400

    Remove debug

--- cmd/main.go
+++ cmd/main.go
@@ -8,32 +8,26 @@ import (
 	"github.com/rcbgr/node-example/pkg/node"
 )
 
+const (
+	usdcContract = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+	maycContract = "0x60e4d786628fea6478f785a6d7e704777c86a7c6" // Mutant Ape Yacht Club: MAYC Token
+)
+
 func main() {
 
-	usdcContract := "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-
-	contract := "0x60e4d786628fea6478f785a6d7e704777c86a7c6" // Mutant Ape Yacht Club: MAYC Token
-
-	tokens, err := node.NodeNftApiTokensByContract(contract)
+	contractTokens, err := node.NodeNftApiTokensByContract(maycContract)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(len(tokens))
-
-	fmt.Println(fmt.Sprintf("Tokens: %d", len(tokens)))
-
 	var owners []string
-
-	for _, token := range tokens {
-		owner, err := node.NodeNftApiTokenOwnerByContractAndToken(contract, token)
-		if err != nil {
+	for _, token := range contractTokens {
+		if owner, err := node.NodeNftApiTokenOwnerByContractAndToken(maycContract, token); err != nil {
 			fmt.Println(err)
+		} else {
+			owners = append(owners, owner)
 		}
-		owners = append(owners, owner)
 	}
-
-	fmt.Println(fmt.Sprintf("Owner addresses: %d", len(owners)))
 
 	f, err := os.Create("balances.csv")
 	if err != nil {
@@ -41,28 +35,15 @@ func main() {
 	}
 
 	w := bufio.NewWriter(f)
-
 	for _, owner := range owners {
-
-		contractAssets, nativeAssets, err := node.NodeAdvancedApiBalanceByContract(
+		if contractAssets, nativeAssets, err := node.NodeAdvancedApiBalanceByContract(
 			usdcContract,
 			owner,
-		)
-
-		if err != nil {
+		); err != nil {
 			fmt.Println(err)
+		} else {
+			w.WriteString(fmt.Sprintf("%s,%0.18f,%f\n", owner, nativeAssets, contractAssets))
 		}
-
-		w.WriteString(fmt.Sprintf("%s,%0.18f,%f\n", owner, nativeAssets, contractAssets))
-
-		fmt.Println(
-			fmt.Sprintf(
-				"Address: %s - USDC balance: %f - native balance: %0.18f",
-				owner,
-				contractAssets,
-				nativeAssets,
-			),
-		)
 	}
 
 	w.Flush()
